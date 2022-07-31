8e29a872844671f37061a62bc5bf8935b34e1a80
Emad Aemi <66124566+EmadAemi@users.noreply.github.com>
2022-07-31 23:20:33 +0430

    Add files via upload

---
+++ 1 - Hangman.py
@@ -0,0 +1,20 @@
+import random
+words = ['book', 'tree', 'elephent', 'airplane', 'tiger', 'apple']
+temp = words[random.randint(0, len(words)-1)]
+chances = 2*len(temp)
+answer = []
+for i in range(len(temp)): answer.append('-')
+word = []
+for i in range(len(temp)): word.append(temp[i])
+while chances>0:
+    for j in range(len(answer)): print(answer[j], end=' ')
+    print('(',chances,'♡ )')
+    char = input("Char: ")
+    if char in word:
+        answer[word.index(char)] = char
+        word[word.index(char)] = ''
+        if '-' not in answer: break
+    else: chances -= 1
+if chances==0: print("**************GAME OVER**************")
+else: print("**************WON**************")
+
